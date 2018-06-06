7469de94f056c583c82432928a0239bc8d914634
SAPDRP Server <edwin@sanandreaspdrp.com>
2018-06-06 19:57:18 -0400

    t

---
+++ A_SERVER_START.cmd
@@ -0,0 +1,14 @@
+color 2
+title SAPDRP SERVERS
+cls
+@echo off
+echo ================================================================
+echo                     WELCOME TO SAPDRP SERVERS
+echo                    HIT ANY KEY TO START SERVER
+echo                     LOADING PLEASE WAIT......
+echo Current time 
+TIME [/T] 
+echo =================================================================
+pause
+color 7
+C:\Users\nerfr\Desktop\serverels\run.cmd +exec server.cfg
